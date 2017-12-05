a92713bf11959a207666fa14db05fa9a43630311
John Wojciechowski <jwojo13@gmail.com>
2017-12-05 15:54:07 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -53,7 +53,10 @@ class Song
   def self.new_from_filename(filename)
     split_file = filename.split(" - ")
     artist = split_file[0]
-    
+    song_name = split_file[1].split(".")[0]
+    song = self.new
+    song.name = song_name
+    song.
 end
 
 end
